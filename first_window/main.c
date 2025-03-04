3424bc0e7776f15e0e35b7c8198382f73d6f3de2
Cristhian <cristhianjhl.web@gmail.com>
2025-03-03 21:19:10 -0500

    update: clang formatter properties to use allman breakbeforebraces

--- first_window/main.c
+++ first_window/main.c
@@ -8,7 +8,8 @@
 const int SCREEN_WIDTH = 600;
 const int SCREEN_HEIGHT = 400;
 
-typedef struct State {
+typedef struct State
+{
     SDL_Window *window;
     SDL_Renderer *renderer;
     bool is_running;
@@ -16,8 +17,10 @@ typedef struct State {
 
 state_t state;
 
-bool init(void) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+bool init(void)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         printf("SDL Initialization failed %s\n", SDL_GetError());
         return false;
     }
@@ -29,13 +32,15 @@ bool init(void) {
         SCREEN_WIDTH,
         SCREEN_HEIGHT,
         SDL_WINDOW_SHOWN);
-    if (state.window == NULL) {
+    if (state.window == NULL)
+    {
         printf("SDL Window Creation Failed %s\n", SDL_GetError());
         return false;
     }
 
     state.renderer = SDL_CreateRenderer(state.window, -1, SDL_RENDERER_ACCELERATED);
-    if (state.renderer == NULL) {
+    if (state.renderer == NULL)
+    {
         printf("SDL Renderer Creation Failed %s\n", SDL_GetError());
         return false;
     }
@@ -44,17 +49,20 @@ bool init(void) {
     return true;
 }
 
-void render(void) {
+void render(void)
+{
     SDL_SetRenderDrawColor(state.renderer, 190, 190, 190, 0xFF);
     SDL_RenderClear(state.renderer);
 }
 
-void present(void) {
+void present(void)
+{
     // UPDATE/PRESENT THE SCREEN.
     SDL_RenderPresent(state.renderer);
 }
 
-void cleanup() {
+void cleanup()
+{
     SDL_DestroyRenderer(state.renderer);
     SDL_DestroyWindow(state.window);
     state.renderer = NULL;
@@ -62,10 +70,12 @@ void cleanup() {
     SDL_Quit();
 }
 
-int main(int argv, char *argc[]) {
+int main(int argv, char *argc[])
+{
     memset(&state, 0, sizeof(state_t));
 
-    if (!init()) {
+    if (!init())
+    {
         SDL_Log("Game initialization failed.\n");
         exit(1);
     }
@@ -76,10 +86,13 @@ int main(int argv, char *argc[]) {
     SDL_Event event;
 
     // GAME LOOP.
-    while (state.is_running) {
+    while (state.is_running)
+    {
         // INPUT HANDLER.
-        while (SDL_PollEvent(&event) != 0) {
-            switch (event.type) {
+        while (SDL_PollEvent(&event) != 0)
+        {
+            switch (event.type)
+            {
                 case SDL_QUIT:
                     state.is_running = false;
                     break;
